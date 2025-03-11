6f62a9b8358aca4d0adaa0d16c381d9ef32f3da8
jsr0000 <josh.regnart@gmail.com>
2025-03-11 15:27:45 +0000

    setup rebase token pool contract

---
+++ src/RebaseTokenPool.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {IRebaseToken} from "src/Interfaces/IRebaseToken.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract RebaseTokenPool {}
