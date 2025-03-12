628225881277fdecefe26fbb628246f95754cfd6
jsr0000 <josh.regnart@gmail.com>
2025-03-12 15:09:09 +0000

    bridge tokens setup

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "lib/openzeppelin-contracts/lib/forge-std/src/Test.sol";
+import {Test, console} from "lib/openzeppelin-contracts/lib/forge-std/src/Test.sol";
 import {CCIPLocalSimulatorFork, Register} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 import {RebaseToken} from "src/RebaseToken.sol";
 import {Vault} from "src/Vault.sol";
@@ -13,9 +13,12 @@ import {RegistryModuleOwnerCustom} from "lib/ccip/contracts/src/v0.8/ccip/tokenA
 import {TokenAdminRegistry} from "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 import {TokenPool} from "lib/chainlink-local/lib/ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {RateLimiter} from "lib/chainlink-local/lib/ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+import {Client} from "lib/chainlink-local/lib/ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "lib/chainlink-local/lib/ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 
 contract CrossChainTest is Test {
     address public owner = makeAddr("owner");
+    address public user = makeAddr("user");
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
 
@@ -129,5 +132,93 @@ contract CrossChainTest is Test {
         uint64[] memory remoteChainSelectorsToRemove = new uint64[](0);
         localPool.applyChainUpdates(remoteChainSelectorsToRemove, chains);
         vm.stopPrank();
+    }
+
+    function bridgeTokens(
+        uint256 amountToBridge,
+        uint256 localFork,
+        uint256 remoteFork,
+        Register.NetworkDetails memory localNetworkDetails,
+        Register.NetworkDetails memory remoteNetworkDetails,
+        RebaseToken localToken,
+        RebaseToken remoteToken
+    ) public {
+        // Create the message to send tokens cross-chain
+        vm.selectFork(localFork);
+        vm.startPrank(user);
+        Client.EVMTokenAmount[]
+            memory tokenToSendDetails = new Client.EVMTokenAmount[](1);
+        Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({
+            token: address(localToken),
+            amount: amountToBridge
+        });
+        tokenToSendDetails[0] = tokenAmount;
+        // Approve the router to burn tokens on users behalf
+        IERC20(address(localToken)).approve(
+            localNetworkDetails.routerAddress,
+            amountToBridge
+        );
+
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(user), // we need to encode the address to bytes
+            data: "", // We don't need any data for this example
+            tokenAmounts: tokenToSendDetails, // this needs to be of type EVMTokenAmount[] as you could send multiple tokens
+            extraArgs: "", // We don't need any extra args for this example
+            feeToken: localNetworkDetails.linkAddress // The token used to pay for the fee
+        });
+        // Get and approve the fees
+        vm.stopPrank();
+        // Give the user the fee amount of LINK
+        ccipLocalSimulatorFork.requestLinkFromFaucet(
+            user,
+            IRouterClient(localNetworkDetails.routerAddress).getFee(
+                remoteNetworkDetails.chainSelector,
+                message
+            )
+        );
+        vm.startPrank(user);
+        IERC20(localNetworkDetails.linkAddress).approve(
+            localNetworkDetails.routerAddress,
+            IRouterClient(localNetworkDetails.routerAddress).getFee(
+                remoteNetworkDetails.chainSelector,
+                message
+            )
+        ); // Approve the fee
+        // log the values before bridging
+        uint256 balanceBeforeBridge = IERC20(address(localToken)).balanceOf(
+            user
+        );
+        console.log("Local balance before bridge: %d", balanceBeforeBridge);
+
+        IRouterClient(localNetworkDetails.routerAddress).ccipSend(
+            remoteNetworkDetails.chainSelector,
+            message
+        ); // Send the message
+        uint256 sourceBalanceAfterBridge = IERC20(address(localToken))
+            .balanceOf(user);
+        console.log("Local balance after bridge: %d", sourceBalanceAfterBridge);
+        assertEq(
+            sourceBalanceAfterBridge,
+            balanceBeforeBridge - amountToBridge
+        );
+        vm.stopPrank();
+
+        vm.selectFork(remoteFork);
+        // Pretend it takes 15 minutes to bridge the tokens
+        vm.warp(block.timestamp + 900);
+        // get initial balance on Arbitrum
+        uint256 initialArbBalance = IERC20(address(remoteToken)).balanceOf(
+            user
+        );
+        console.log("Remote balance before bridge: %d", initialArbBalance);
+        ccipLocalSimulatorFork.switchChainAndRouteMessage(remoteFork);
+
+        console.log(
+            "Remote user interest rate: %d",
+            remoteToken.getUserInterestRate(user)
+        );
+        uint256 destBalance = IERC20(address(remoteToken)).balanceOf(user);
+        console.log("Remote balance after bridge: %d", destBalance);
+        assertEq(destBalance, initialArbBalance + amountToBridge);
     }
 }
